299c1d7df625b6104a30b55b48821ef04b0d3ad5
Sauryadeep Pal <sp04012000@gmail.com>
2019-01-06 09:24:16 +0800

    Update TextToMusic.py
    - removed useless comments and code
    - hardcoded chord_dict instead of generating it

--- TextToMusic.py
+++ TextToMusic.py
@@ -1,18 +1,13 @@
-# import threading
-import _thread
+import _thread  # TODO: Use threading instead
 import playsound
 import random
 from tkinter import *
 from Progressions import Progressions
 
 
-def num_to_chord():
-    num = [i for i in range(1, 25)]
-    chords = [i for i in "A A# B C C# D D# E F F# G G# Am A#m Bm Cm C#m Dm D#m Em Fm F#m Gm G#m".split(" ")]
-    chord_dict = {}
-    for i in range(0, 24):
-        chord_dict.update({num[i]: chords[i]})
-    return chord_dict
+chord_dict = {1: "A", 2: "A#", 3: "B", 4: "C", 5: "C#", 6: "D", 7: "D#", 8: "E", 9: "F", 10: "F#", 11: "G",
+              12: "G#", 13: "Am", 14: "A#m", 15: "Bm", 16: "Cm", 17: "C#m", 18: "Dm", 19: "D#m", 20: "E#m",
+              21: "Fm", 22: "F#m", 23: "Gm", 24: "G#m"}
 
 
 def text_to_music(phrase):
@@ -24,14 +19,12 @@ def text_to_music(phrase):
     num_2 = [int(y) for y in range(1, 25)]
 
     start_chord = random.randint(1, 24)
-
     progression = Progressions(start_chord, [start_chord], None)
-
     progression.generate_progression()
 
     chord_count = len(progression.chords)
 
-    # Deletes extra chords if len(chords) exceeds char_count and adds extra chords if char_count exceeds len(chords)
+    # Deletes extra chords or adds them
     if char_count < chord_count:
         del progression.chords[char_count:]
     elif chord_count < char_count:
@@ -42,21 +35,14 @@ def text_to_music(phrase):
         progression.extra = extra_chords
         progression.add_chords()
 
-    # print(str(progression.chords))                    for console
-    # print("Playing chords: ")                         for console
     chords_playing = []
     for i in progression.chords:
-        if i in num_to_chord().keys():
-            chords_playing.append(num_to_chord()[i])
+        if i in chord_dict.keys():
+            chords_playing.append(chord_dict[i])
         filename = "audio files/{}__{}.wav".format(num_1[i-1], num_2[i-1])
         playsound.playsound(filename)
-    # print(", ".join(str(x) for x in chords_playing))  for console
-    # TODO: Display chords dynamically instead at the end of each progression.
     chords_display.insert(END, chords_playing)
 
-
-# if __name__ == '__main__':                            for console
-#     text_to_music(str(input("Enter phrase: ")))       for console
 
 root = Tk()
 root.title("Text to Music")
@@ -66,7 +52,9 @@ text_field_name = Label(input_frame, text="Enter phrase: ")
 text_field_name.grid(row=0, column=0, padx=5)
 text_field = Entry(input_frame)
 text_field.grid(row=0, column=1)
-play_prog_button = Button(input_frame, text="Play Progression!", command=lambda: _thread.start_new_thread(text_to_music, (text_field.get(),)), bg="blue", fg="white")
+play_prog_button = Button(input_frame, text="Play Progression!",
+                          command=lambda: _thread.start_new_thread(text_to_music, (text_field.get(),)), bg="blue",
+                          fg="white")
 play_prog_button.grid(row=0, column=2, padx=5)
 output_frame = Frame(root)
 output_frame.grid(row=1)
@@ -81,39 +69,3 @@ scrollbar.config(command=chords_display.yview)
 
 
 root.mainloop()
-
-# TODO: Figure out the correct code that'll run with threading instead of _thread
-# class GUIThread(threading.Thread):
-#     def __init__(self, text_output):
-#         threading.Thread.__init__(self)
-#         self.text_output = text_output
-#
-#     def run(self):
-#         text_to_music(self.text_output)
-#
-#
-# root = Tk()
-# root.title("Text to Music")
-# input_frame = Frame(root)
-# input_frame.grid(row=0)
-# text_field_name = Label(input_frame, text="Enter phrase: ")
-# text_field_name.grid(row=0, column=0, padx=5)
-# text_field = Entry(input_frame)
-# text_field.grid(row=0, column=1)
-# output_frame = Frame(root)
-# output_frame.grid(row=1)
-# chord_title = Label(output_frame, text="Chords in this progression: ")
-# chord_title.grid(row=0, column=0)
-# chords_display = Text(output_frame, height=2, width=40)
-# chords_display.grid(row=1, column=0)
-# scrollbar = Scrollbar(output_frame)
-# scrollbar.grid(row=1, column=2)
-# chords_display.config(yscrollcommand=scrollbar.set)
-# scrollbar.config(command=chords_display.yview)
-# phrase = text_field.get()
-# GUI = GUIThread(phrase)
-# play_prog_button = Button(input_frame, text="Play Progression!", command=GUI.start(), bg="blue", fg="white")
-# play_prog_button.grid(row=0, column=2, padx=5)
-#
-#
-# root.mainloop()
