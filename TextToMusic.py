d43a984dabb2860b9e2ae154e669fe610bf1bb85
Sauryadeep Pal <sp04012000@gmail.com>
2019-05-27 20:42:12 +0800

    Update TextToMusic.py
    - completely removed main() and replaced it with play_prog() and export_prog().
    - TextToMusic has no imported external libraries. As such, the playsound function was moved to Resources.py.
    - the instance of the progression class is now declared in the zeroth level.

--- TextToMusic.py
+++ TextToMusic.py
@@ -1,51 +1,24 @@
-import playsound
-import random
 from Resources import Progression, GUI
 
 
-def main():
-    """
-    This function is responsible for correctly generating the progression as well as managing both the progression and
-    part of the GUI window.
-    """
-
-    # reset the GUI chords display and disable play_prog_button
-    display_window.clear_chords_display()
-    display_window.button_status(display_window.play_prog_button, 0)
-
-    # generate non-user inputs
-    start_chord = random.randint(1, 24)
-    serial_number = range(47845, 47869)
-    chord_number = range(1, 25)
-
-    # create a progression as an instance of the 'Progression' class
-    progression = Progression(display_window.text_field.get(), start_chord, [start_chord], [], serial_number,
-                              chord_number)
-
-    # manage the progression by calling the class methods in the correct order
-    progression.generate_progression()
-    progression.manage_chords()
-    progression.insert_chords_to_display()
-
-    # call the method that displays the chords in the GUI window
-    display_window.display_chords()
-
-    # iterate over every value in 'progression.chords' and play the matching audio file
-    for i in progression.chords:
-        try:
-            filename = "audio files/{}__{}.wav".format(progression.num_1[i-1], progression.num_2[i - 1])
-            playsound.playsound(filename)
-        except FileNotFoundError:
-            print('Error: audio files not found.')
-
-    # re-enable play_prog_button and 'export_prog_button'.
-    display_window.button_status(display_window.play_prog_button, 1)
-    display_window.button_status(display_window.export_prog_button, 1)
-
-    # Enable progression to be exported to XML
-    display_window.exp_func = progression.export_progression()
+def play_prog():
+    progression.phrase = gui.phrase.get()
+    progression.create_inputs()
+    chord_array = progression.edit_progression()
+    gui.chords_display.config(text=" / ".join(chord_array[0]))
+    progression.play_progression()
+    gui.play_prog_button.config(state='normal')
+    return gui.export_prog_button.config(state='normal')
 
 
-# generate the GUI window
-display_window = GUI(main, Progression.chords_playing)
-display_window.mainloop()
+def export_prog():
+    gui.info.config(text="Exporting...")
+    progression.export_progression()
+    return gui.info.config(text="Done.")
+
+
+if __name__ == '__main__':
+    gui = GUI(play_func=play_prog, exp_func=export_prog)
+    progression = Progression()
+    gui.display_window()
+    gui.mainloop()
